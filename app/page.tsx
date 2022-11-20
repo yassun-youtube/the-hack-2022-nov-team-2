3e84b9276b1c07877f622d09ffb47a04114803b6
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-11-20 21:45:41 +0900

    header部分を実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Page() {
-  return (
-    <h1 className="text-3xl text-blue-600 font-bold underline">Poke Blend</h1>
-  );
+  return <button className="btn btn-primary">daisyUi button</button>;
 }
---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav className="p-2 flex  row space-x-4 justify-between w-full ">
+        <h1 className="text-5xl text-blue-600 font-bold">Poke Blend</h1>
+
+        <ul className="flex items-center justify-end space-x-2">
+          <li>
+            <button className="btn btn-outline btn-primary glass btn-sm">
+              <Link href="/">Home</Link>
+            </button>
+          </li>
+          <li>
+            <button className="btn btn-outline btn-info glass btn-sm">
+              <Link href="/about">About</Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
