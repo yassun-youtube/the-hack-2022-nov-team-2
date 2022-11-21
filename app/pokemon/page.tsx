f234780629af44f918b047685de56f9765b665fb
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-11-21 23:10:04 +0900

    Cardっぽく

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -13,12 +13,22 @@ export default async function PokemonTop() {
   return (
     <div>
       <h1 className="text-3xl">Pokemon</h1>
-      <Image
-        src={pikachuData.sprites.front_default}
-        alt={pikachuData.name}
-        width={100}
-        height={100}
-      />
+      <div className="card w-60  shadow-2xl flex items-center bg-yellow-100">
+        <div className="relative w-40 h-40 object-fill px-10 pt-10">
+          <Image
+            src={pikachuData.sprites.other["official-artwork"].front_default}
+            alt={pikachuData.name}
+            fill
+          />
+        </div>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{pikachuData.name}</h2>
+          <h3 className="font-semibold">[Types]</h3>
+          {pikachuData.types.map((type) => (
+            <p>{type.type.name}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
