d7363273c6bf88d2dca8b359fb7681cae4e492db
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-12-03 23:54:32 +0900

    colorをtailwindConfigに定義

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      pokeBlend1: "#EEB2BF",
+      pokeBlend2: "#D41E45",
+      pokeBlend3: "#1E1848",
+      pokeBlend4: "#E2FCEF",
+      pokeBlend5: "#9DC0BC",
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
