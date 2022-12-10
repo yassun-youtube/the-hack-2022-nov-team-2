95a5cbd55ad28ff1d72733deddcc1fc07efffac6
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-12-10 19:14:47 +0900

    pokemonNameの日本語化

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,133 +1,175 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import Image from "next/image";
 import useSWR from "swr";
-import { PokemonResponse } from "../schema";
+import { PokemonResponse } from "../types/pokemonResponse";
+import { PokemonDetailResponse } from "../types/pokemonDetailResponse";
 
 type PokemonCardProps = {
   pokemonUrl: string;
 };
 
+const JACODE = "ja-Hrkt";
+
 export const PokemonCard = ({ pokemonUrl }: PokemonCardProps) => {
+  const pokemonId = useMemo(() => {
+    if (!pokemonUrl) return;
+
+    const arr = pokemonUrl.split("/");
+    return arr[arr.length - 2];
+  }, [pokemonUrl]);
+
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data: pokemon, error } = useSWR<PokemonResponse>(pokemonUrl, fetcher);
-  if (error) return <div>failed to load</div>;
-  if (!error && !pokemon)
+  const { data: pokemonData, error } = useSWR<PokemonResponse>(
+    pokemonUrl,
+    fetcher
+  );
+
+  const { data: pokemonDetailData, error: detailError } =
+    useSWR<PokemonDetailResponse>(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`,
+      fetcher
+    );
+
+  const pokemonName = useMemo(() => {
+    if (!pokemonDetailData) return;
+
+    return pokemonDetailData?.names?.find((n) => n.language.name === JACODE)
+      ?.name;
+  }, [pokemonDetailData]);
+
+  const pokemonGenus = useMemo(() => {
+    if (!pokemonDetailData) return;
+
+    return pokemonDetailData?.genera?.find((n) => n.language.name === JACODE)
+      ?.genus;
+  }, [pokemonDetailData]);
+
+  if (error || detailError) return <div>failed to load</div>;
+  if (!error && !pokemonData && !pokemonDetailData)
     return (
       <div className="flex justify-center">
         <div className="animate-spin h-8 w-8 bg-blue-300 rounded-xl"></div>
       </div>
     );
 
-  return pokemon ? (
-    <div className="flex justify-start items-center border-4 border-pokeBlend1 rounded-lg p-4 cursor-pointer">
-      <div className="card-body  text-center">
-        <h1 className="card-title">{pokemon.name}</h1>
+  return pokemonData ? (
+    <div className="flex justify-between items-center border-4 border-pokeBlend1 rounded-lg p-4 cursor-pointer">
+      <div className="flex flex-col gap-3 ml-3">
+        <div className="card-body text-center ">
+          <h1 className="card-title">{pokemonName}</h1>
+        </div>
+        {pokemonGenus && <p className="text-center">[{pokemonGenus}]</p>}
       </div>
-      <div className="relative w-40 h-40 object-fill px-10 pt-10">
-        <Image
-          src={pokemon.sprites.other["official-artwork"].front_default}
-          alt={pokemon.name}
-          loading="eager"
-          fill
-          sizes="150"
-          priority
-        />
-      </div>
-
-      {pokemon.sprites.other["dream_world"].front_default && (
+      <div className="flex flex-row gap-3">
         <div className="relative w-40 h-40 object-fill px-10 pt-10">
           <Image
-            src={pokemon.sprites.other["dream_world"].front_default ?? ""}
-            alt={pokemon.name}
+            src={pokemonData.sprites.other["official-artwork"].front_default}
+            alt={pokemonData.name}
             loading="eager"
             fill
             sizes="150"
             priority
           />
         </div>
-      )}
 
-      {pokemon.sprites.versions["generation-ii"].crystal.front_default && (
-        <div className="relative w-40 h-40 object-fill px-10 pt-10">
-          <Image
-            src={
-              pokemon.sprites.versions["generation-ii"].crystal.front_default
-            }
-            alt={pokemon.name}
-            loading="eager"
-            fill
-            sizes="150"
-            priority
-          />
-        </div>
-      )}
+        {pokemonData.sprites.other["dream_world"].front_default && (
+          <div className="relative w-40 h-40 object-fill px-10 pt-10">
+            <Image
+              src={pokemonData.sprites.other["dream_world"].front_default ?? ""}
+              alt={pokemonData.name}
+              loading="eager"
+              fill
+              sizes="150"
+              priority
+            />
+          </div>
+        )}
 
-      {pokemon.sprites.versions["generation-iii"]["ruby-sapphire"]
-        .front_default && (
-        <div className="relative w-40 h-40 object-fill px-10 pt-10">
-          <Image
-            src={
-              pokemon.sprites.versions["generation-iii"]["ruby-sapphire"]
-                .front_default
-            }
-            alt={pokemon.name}
-            loading="eager"
-            fill
-            sizes="150"
-          />
-        </div>
-      )}
+        {pokemonData.sprites.versions["generation-ii"].crystal
+          .front_default && (
+          <div className="relative w-40 h-40 object-fill px-10 pt-10">
+            <Image
+              src={
+                pokemonData.sprites.versions["generation-ii"].crystal
+                  .front_default
+              }
+              alt={pokemonData.name}
+              loading="eager"
+              fill
+              sizes="150"
+              priority
+            />
+          </div>
+        )}
 
-      {pokemon.sprites.versions["generation-iv"]["diamond-pearl"]
-        .front_default && (
-        <div className="relative w-40 h-40 object-fill px-10 pt-10">
-          <Image
-            src={
-              pokemon.sprites.versions["generation-iv"]["diamond-pearl"]
-                .front_default
-            }
-            alt={pokemon.name}
-            loading="eager"
-            fill
-            sizes="150"
-          />
-        </div>
-      )}
+        {pokemonData.sprites.versions["generation-iii"]["ruby-sapphire"]
+          .front_default && (
+          <div className="relative w-40 h-40 object-fill px-10 pt-10">
+            <Image
+              src={
+                pokemonData.sprites.versions["generation-iii"]["ruby-sapphire"]
+                  .front_default
+              }
+              alt={pokemonData.name}
+              loading="eager"
+              fill
+              sizes="150"
+            />
+          </div>
+        )}
 
-      {pokemon.sprites.versions["generation-v"]["black-white"].animated
-        .front_default && (
-        <div className="relative w-40 h-40 object-fill px-10 pt-10">
-          <Image
-            src={
-              pokemon.sprites.versions["generation-v"]["black-white"].animated
-                .front_default
-            }
-            alt={pokemon.name}
-            loading="eager"
-            fill
-            sizes="150"
-          />
-        </div>
-      )}
+        {pokemonData.sprites.versions["generation-iv"]["diamond-pearl"]
+          .front_default && (
+          <div className="relative w-40 h-40 object-fill px-10 pt-10">
+            <Image
+              src={
+                pokemonData.sprites.versions["generation-iv"]["diamond-pearl"]
+                  .front_default
+              }
+              alt={pokemonData.name}
+              loading="eager"
+              fill
+              sizes="150"
+            />
+          </div>
+        )}
 
-      {pokemon.sprites.versions["generation-vi"]["omegaruby-alphasapphire"]
-        .front_default && (
-        <div className="relative w-40 h-40 object-fill px-10 pt-10">
-          <Image
-            src={
-              pokemon.sprites.versions["generation-vi"][
-                "omegaruby-alphasapphire"
-              ].front_default
-            }
-            alt={pokemon.name}
-            loading="eager"
-            fill
-            sizes="150"
-          />
-        </div>
-      )}
+        {pokemonData.sprites.versions["generation-v"]["black-white"].animated
+          .front_default && (
+          <div className="relative w-40 h-40 object-fill px-10 pt-10">
+            <Image
+              src={
+                pokemonData.sprites.versions["generation-v"]["black-white"]
+                  .animated.front_default
+              }
+              alt={pokemonData.name}
+              loading="eager"
+              fill
+              sizes="150"
+            />
+          </div>
+        )}
+
+        {pokemonData.sprites.versions["generation-vi"][
+          "omegaruby-alphasapphire"
+        ].front_default && (
+          <div className="relative w-40 h-40 object-fill px-10 pt-10">
+            <Image
+              src={
+                pokemonData.sprites.versions["generation-vi"][
+                  "omegaruby-alphasapphire"
+                ].front_default
+              }
+              alt={pokemonData.name}
+              loading="eager"
+              fill
+              sizes="150"
+            />
+          </div>
+        )}
+      </div>
     </div>
   ) : (
     <h1>Not found Pokemon</h1>
