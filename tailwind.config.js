b9118350ee831b6bca5fe07ba05128d50c3625a4
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-12-19 00:47:29 +0900

    fix:animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,12 @@ module.exports = {
     extend: {
       keyframes: {
         wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
         },
       },
       animation: {
-        'waving': 'wave 4s linear infinite',
+        waving: "wave 10s linear infinite",
       },
     },
     colors: {
